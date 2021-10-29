f3d8826291e85f017212f46d5ed560c00e3d055c
Code Palace <codepalace@fastmail.se>
2021-10-29 19:24:47 +0200

    Initial Commit

---
+++ CurrencyConvo/CurrencyConvoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConvoApp.swift
+//  CurrencyConvo
+//
+//  Created by Federico on 29/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConvoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
